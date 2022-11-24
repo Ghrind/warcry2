c137ea116159e2035384a57b640adeed99a8b4b1
Benoît Dinocourt <ghrind@gmail.com>
2022-11-24 17:51:20 +0100

    Initial proof of concept

--- src/App.js
+++ src/App.js
@@ -1,24 +1,88 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import { createContext } from 'react'
+import { useContext } from 'react'
+import { useState } from 'react'
+import { useEffect } from 'react'
+import { readRemoteFile } from "react-papaparse";
+import qs from "qs";
+import { createBrowserHistory } from 'history'
+
+const profilesContext = createContext([]);
+
+function Compendium() {
+  const profiles = useContext(profilesContext)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ul>
+      { profiles.map(f => <li key={f.profileId}>{f.name}</li>) }
+    </ul>
+  )
+}
+
+function RosterLine(props) {
+  return (
+    <li>{props.profileId}: {props.profile && props.profile.name}</li>
+  )
+}
+
+function Roster() {
+  const [profileIds, setProfileIds] = useState([]);
+  const profiles = useContext(profilesContext)
+
+  const history = createBrowserHistory();
+
+  useEffect(() => {
+    const filterParams = history.location.search.substr(1);
+    const filtersFromParams = qs.parse(filterParams);
+    if (filtersFromParams.p) {
+      setProfileIds(filtersFromParams.p.split(","));
+    }
+  }, []);
+
+  useEffect(() => {
+    history.push(`?p=${profileIds.join(',')}`);
+  }, [profileIds]);
+
+  const removeFighter = () => {
+    return setProfileIds(["4", "5"])
+  }
+
+  return (
+    <>
+      <button onClick={removeFighter}>apply</button>
+      <ul>
+        {profileIds.map(id => <RosterLine profileId={id} profile={profiles.filter(p => p.profileId === id)[0]} />)}
+      </ul>
+    </>
+  )
+}
+
+function App() {
+  const [profiles, setProfiles] = useState([]);
+
+  useEffect(() => {
+    if (profiles.length !== 0) {
+      return
+    }
+
+    readRemoteFile(
+      "https://docs.google.com/spreadsheets/d/1vdbVXRwONKkx59xyKy0eopc8gjeFQLM3dZZM84HPVE8/export?format=csv",
+      {
+        header: true,
+        complete: (results) => {
+          setProfiles(results.data);
+        },
+      }
+    )
+  })
+
+  return (
+    <profilesContext.Provider value={profiles}>
+      <h1>Test</h1>
+      <Compendium />
+      <Roster />
+    </profilesContext.Provider>
   );
 }
 
