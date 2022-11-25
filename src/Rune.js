3950527b2156869d04ea78b707056fc71a3ff26d
Benoît Dinocourt <ghrind@gmail.com>
2022-11-25 19:36:34 +0100

    Add runes

--- src/Rune.js
+++ src/Rune.js
@@ -1,4 +1,39 @@
-export function Rune() {
-  // TODO change me
-  return(<span></span>)
+import './runes.css'
+
+const mapping = {
+  agile: '',
+  attacks: '',
+  axe: '',
+  beast: '',
+  berserker: '',
+  blast: '',
+  champion: '',
+  dagger: '',
+  damage: '',
+  destroyer: '',
+  elite: '',
+  fangs: '',
+  ferocious: '',
+  frenzied: '',
+  leader: '',
+  move: '',
+  mystic: '',
+  range: '',
+  'ranged weapon': '',
+  'reach weapon': '',
+  spear: '',
+  strength: '',
+  sword: '',
+  toughness: '',
+  warrior: '',
+  wounds: '',
+}
+
+export function Rune(props) {
+  const character = mapping[props.name.toLowerCase()];
+  const className = character ? 'rune' : ''
+
+  return (
+    <span className={className}>{character ? character : props.name}</span>
+  )
 }
